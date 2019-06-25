14e523b18f5b9c3a575e7f8b97704983fe023a6a
LucaFinizio <51985003+LucaFinizio@users.noreply.github.com>
2019-06-25 15:01:54 +0200

    Update mainClass.java

--- EserciziTerzaPlaylist/esVideo9/mainClass.java
+++ EserciziTerzaPlaylist/esVideo9/mainClass.java
@@ -1,7 +1,13 @@
-import java.awt.*;
-import javax.swing.*;
+package esVideo09_Grafica;
 
 
+/**Esercizio playlist 3, video 9, minuto 8:42
+ * L'esercizio chiede di far tornare il JButon del suo colore normale quando il mouse esce da esso (dopo essere
+ * cambiato di colore all'entrata del cursore su di esso)
+ * 
+ * @author Luca Finizio
+ *
+ */
 public class mainClass {
 
 	public static void main(String[] args) {
