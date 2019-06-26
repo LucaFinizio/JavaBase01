149c0c337ed366e2d663711f9415197492e7c9f8
LucaFinizio <51985003+LucaFinizio@users.noreply.github.com>
2019-06-26 09:42:48 +0200

    Update esVideo25.java

--- EserciziPrimaPlaylist/esVideo25.java
+++ EserciziPrimaPlaylist/esVideo25.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * Esercizio playlist 1, video 25, minuto 7:22
+ * L'esercizio chiede di riprendere gli esempi precedenti in cui si davano valori fissi e implementarli tramite la
+ * classe Scanner e i comandi next di vario tipo
+ * 
+ * @author Luca Finizio
+ *
+ */
 public class esVideo25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Creo l'oggetto che legge da tastiera
 		Scanner tastiera=new Scanner(System.in);
 		
 		//Chiedo il nome in input
