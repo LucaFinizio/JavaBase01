2bf4cfb31978abb285b72e43d3de140c25e148ad
LucaFinizio <51985003+LucaFinizio@users.noreply.github.com>
2019-06-26 09:42:08 +0200

    Update esVideo16.java

--- EserciziPrimaPlaylist/esVideo16.java
+++ EserciziPrimaPlaylist/esVideo16.java
@@ -1,8 +1,26 @@
+/**
+*  Esercizio playlist 1, video 16 minuto 7:58.	
+  
+*  L'esercizio chiede se gli operatori unari in forma pre e postfissa sono applicabili a un char, una 
+*  stringa o un boolean. Andiamo a testare quindi i vari tipi.
+*  
+*  @author Luca Finizio
+*/
 
 public class esVideo16 {
 
+	
+	/**
+	 * x variabile usata per testare il tipo byte;
+	 * y variabile usata per testare il tipo short;
+	 * z variabile usata per testare il tipo long;
+	 * f variabile usata per testare il tipo float;
+	 * c variabile usata per testare il tipo char;
+	 * vf variabile usata per testare il tipo boolean;
+	 * s variabile usata per testare il tipo String;
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		byte x = 1;
 		System.out.println("byte x++: " + x++);
 		System.out.println("byte ++x: " + ++x);
@@ -26,13 +44,19 @@ public class esVideo16 {
 		System.out.println("float ++f: " + ++f);
 		System.out.println("float f--: " + f--);
 		System.out.println("float --f: " + --f);
+		//NOTA:Quando sottraiamo al float arriviamo a 1.0999999
 		
 		char c = 'A';
 		System.out.println("char c++: " + c++);
 		System.out.println("char ++c: " + ++c);
 		System.out.println("char c--: " + c--);
 		System.out.println("char --c: " + --c);
+		//NOTA: Aggiungendo (o sottraendo) al char, andiamo a spostarci sul carattere successivo (o precedente) in ordine alfabetico
 		
+		
+		/**
+		 *  Non è possibile applicare gli operatori unari ai tipi boolean e String
+		 */
 		/*
 		boolean vf = false;
 		System.out.println("boolean vf++: " + vf++);
